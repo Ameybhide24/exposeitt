82cf2da87c9b450e4780d5f870b256a0dce42da2
Amey Bhide <amey@Ameys-MacBook-Air.local>
2025-05-06 17:26:05 -0700

    change gemini model

--- GemIntegrate/config/gemini.config.js
+++ GemIntegrate/config/gemini.config.js
@@ -1,8 +1,9 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+console.log(process.env.GEMINI_API_KEY);
 
-const textModel = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
+const textModel = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 const visionModel = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
 module.exports = {
